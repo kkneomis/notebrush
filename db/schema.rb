15001c0f5ed8acb8c80bf0b3609ec839ad1bf164
Simeon Kakpovi <skakpovi@gmail.com>
2016-05-22 16:10:57 -0400

    more styling

--- db/schema.rb
+++ db/schema.rb
@@ -43,16 +43,4 @@ ActiveRecord::Schema.define(version: 20160522181751) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table "videos", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "user_id"
-    t.string   "tag"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "vid"
-  end
-
-  add_index "videos", ["user_id"], name: "index_videos_on_user_id"
-
 end
