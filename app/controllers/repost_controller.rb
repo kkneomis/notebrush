5ce0035dfa89bf6ad0c58d9ca377279536c4e508
Simeon Kakpovi <skakpovi@gmail.com>
2016-06-19 23:53:34 -0400

    fixed retweet function

--- app/controllers/repost_controller.rb
+++ app/controllers/repost_controller.rb
@@ -3,8 +3,8 @@ class RepostController < ApplicationController
     
     def share
         @entry = Entry.find(params[:id])
-        @repost_count = Repost.where(:id => @entry.id,reposter_id: current_user.id).size
-        if @repost_count > 0
+        @repost_count = Repost.where(id: @entry.id, reposter_id: current_user.id).size
+        if @repost_count == 0
             @repost = Repost.new()
             @repost.entry_id = @entry.id
             @repost.reposter_id = current_user.id
@@ -13,25 +13,20 @@ class RepostController < ApplicationController
             redirect_to feed_path
             flash.notice = "Video reposted!"
         else
-            redirect_to root_path
+            flash.notice = "You have already shared this post"
+            redirect_to feed_path
         end
     end 
     
     
     def unshare
         @entry = Entry.find(params[:id])
-        @repost_count = Repost.where(:id => @entry.id,reposter_id: current_user.id).size
-        if @repost_count == 0
-            #do something
             @reposts= Repost.where(entry_id: @entry.id, reposter_id: current_user.id) 
             @reposts.each do |repost|
                 repost.destroy
             end
             redirect_to feed_path
             flash.notice = "Video unreposted!"
-        else
-            redirect_to root_path
-        end
     end 
     
 end
